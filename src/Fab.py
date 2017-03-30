341136850f27913ebeb4860d573f07649cdb4b5a
John Zhu <johnzhu@icloud.com>
2017-03-29 23:07:11 -0700

    removal of dumb predictors classes

--- src/Fab.py
+++ src/Fab.py
@@ -17,22 +17,6 @@ import datetime as dt
 import glob
 
 
-class ModeClassifier(object):
-    """
-    A dumb classification model that simply predicts the most highest occurence outcome.
-    """
-
-    def __init__(self):
-        self.mode = None
-
-    def fit(self, X_train, y):
-        self.mode = np.argmax(np.bincount(y))
-
-    def predict(self, X_test):
-        return np.array([self.mode] * len(X_test))
-
-
-
 class Fab(object):
 	'''
 	Fab - FOMC Analyzer Bot
