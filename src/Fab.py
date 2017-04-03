0d10b637de2cfc1bc8e62c1d70f49e055a61ef06
John Zhu <johnzhu@icloud.com>
2017-04-02 22:00:23 -0700

    ported to python 3 to support Textacy

--- src/Fab.py
+++ src/Fab.py
@@ -1,43 +1,3 @@
-# from __future__ import print_function
-from nltk.stem.wordnet import WordNetLemmatizer
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.svm import SVC
-
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.ensemble import GradientBoostingRegressor
-from sklearn.neighbors import KNeighborsRegressor
-
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import TimeSeriesSplit
-from sklearn.model_selection import GridSearchCV
-
-from sklearn.metrics import f1_score, accuracy_score, precision_score, recall_score, roc_auc_score
-
-from sklearn.metrics.regression import mean_absolute_error, mean_squared_error, r2_score
-
-from sklearn.preprocessing import LabelEncoder
-
-from dumb_predictors import MeanRegressor, ModeClassifier
-
-import pandas as pd
-import numpy as np
-import pickle
-import datetime as dt
-import glob
-import sys
-import os.path
-
-from keras.models import Sequential
-from keras.layers import Dense, Activation
-
-np.set_printoptions(formatter={'float_kind':lambda x: "%.4fdf" % x})
-
-
 class Fab(object):
     """
     Fab - FOMC Analyzer Bot
@@ -51,11 +11,11 @@ class Fab(object):
         self.labels = None
         self.last_test = None
         self.last_predict = None
-        self.meeting_statements = self.get_meeting_statements('../data/minutes_df.pickle')
+        self.meeting_statements = self.get_meeting_statements('../data/minutes.pickle')
         self.prices = self.get_prices()
         self.set_labels()
 
-        self.df = None  # a data frame that holds all the X and y's 
+        self.all_data_df = None  # a data frame that holds all the X and y's 
         self.X = None
         self.y = None
 
@@ -64,8 +24,8 @@ class Fab(object):
 
 
     def get_meeting_statements(self, filename='../data/minutes_df.pickle'):
-        with open(filename) as f:
-            print "Loading saved statements"
+        with open(filename, 'rb') as f:
+            print("Loading saved statements")
             return pickle.load(f)
 
 
@@ -74,8 +34,8 @@ class Fab(object):
         # note the timezone issues need to be rechecked prior to running live
 
         if refresh is False and os.path.exists(pickle_path):
-            print "Loading saved prices"
-            with open(pickle_path) as f:
+            print("Loading saved prices")
+            with open(pickle_path, 'rb') as f:
                 return pickle.load(f)
 
         prices = dict()
@@ -97,8 +57,8 @@ class Fab(object):
     def set_labels(self, index=None, pickle_path='../data/labels.pickle', refresh=False):
 
         if refresh is False and os.path.exists(pickle_path):
-            print "Loading saved labels"
-            with open(pickle_path) as f:
+            print("Loading saved labels")
+            with open(pickle_path, 'rb') as f:
                 self.labels = pickle.load(f)
                 return self.labels
 
@@ -118,7 +78,7 @@ class Fab(object):
         y_dfs = dict()
         for key in prices_FOMC:
             y_dfs[key] = prices_FOMC[key].groupby(prices_FOMC[key].index.date).diff().dropna() 
-            y_dfs[key]['fomc-close-MA-4-pct'] = (y_dfs[key]['close-MA-4'] / prices[key].loc[y_dfs[key].index]['close'])
+            y_dfs[key]['fomc-close-MA-4-pct'] = (y_dfs[key]['close-MA-4'] / self.prices[key].loc[y_dfs[key].index]['close'])
 
             # Removes the time from the index since now we are left with one prediction a day
             y_dfs[key].index = y_dfs[key].index.normalize()
@@ -135,11 +95,13 @@ class Fab(object):
             pickle.dump(self.labels, f)
 
 
-    def lemmatize_descriptions(self, meeting_statement):
-        lem = WordNetLemmatizer()
-        lemmatize = lambda d: " ".join(lem.lemmatize(word) for word in d.split())
-        return [lemmatize(desc) for desc in meeting_statement]
-
+    def lemmatize_descriptions(self, meeting_statements):
+        lemmatized = []
+        for statement in meeting_statements:
+            doc = nlp(statement)
+            lemmatized.append(" ".join([token.lemma_ for token in doc]))
+        return lemmatized
+            
 
     def get_vectorizer(self, meeting_statement, num_features=5000):
         vect = TfidfVectorizer(max_features=num_features, stop_words='english')
@@ -177,18 +139,18 @@ class Fab(object):
         X_test = vect.transform(desc_test).toarray()
 
         if self.regression:
-            print "r2\tmse\tmae"
+            print("r2\tmse\tmae")
             for model in models:
                 name = model.__class__.__name__
                 r2, mse, mae = self.run_model(model, X_train, X_test, y_train, y_test)
-                print "%.4f\t%.4f\t%.4f\t%s" % (r2, mse, mae, name)
+                print("%.4f\t%.4f\t%.4f\t%s" % (r2, mse, mae, name))
             return mse
         else:
-            print "auc\tacc\tf1\tprec\trecall"
+            print("auc\tacc\tf1\tprec\trecall")
             for model in models:
                 name = model.__class__.__name__
                 auc_score, acc, f1, prec, rec = self.run_model(model, X_train, X_test, y_train, y_test)
-                print "%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%s" % (auc_score, acc, f1, prec, rec, name)
+                print("%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%s" % (auc_score, acc, f1, prec, rec, name))
             return acc
 
 
@@ -201,19 +163,19 @@ class Fab(object):
         for train_index, test_index in tscv.split(desc):
             desc_train, desc_test = desc[train_index], desc[test_index]
             y_train, y_test = y[train_index], y[test_index]
-            print ""
-            print "Length: train {}, test {}".format(len(train_index), len(test_index))
-            print "Balance: train {}, test {}".format(np.sum(y_train)/float(len(y_train)), 
-                                                      np.sum(y_test)/float(len(y_test))) 
+            print("")
+            print("Length: train {}, test {}".format(len(train_index), len(test_index)))
+            print("Balance: train {}, test {}".format(np.sum(y_train)/float(len(y_train)), 
+                                                      np.sum(y_test)/float(len(y_test))) )
             # print "-----------------------------"
             # print "Without Lemmatization:"
             # self.run_test(models, desc_train, desc_test, y_train, y_test)
 
-            print "-----------------------------"
-            print "With Lemmatization:"
+            print("-----------------------------")
+            print("With Lemmatization:")
             self.run_test(models, self.lemmatize_descriptions(desc_train),
                            self.lemmatize_descriptions(desc_test), y_train, y_test)
-            print "-----------------------------"
+            print("-----------------------------")
 
 
     def predict(self, meeting_statement, timestamp, tickers):
@@ -263,47 +225,27 @@ class Fab(object):
         # sequential.add(Activation('softmax'))
         # sequential.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
 
+
+
+        # build NLP df with features
+#         self.meeting_statements.loc[self.labels[ticker].index]['statements']
+
+
+        # take index from self.labels and build features based on prices
+
+
+
+        # merge the NLP features with the prices features on the index of each ticker
+
+
+
+        # Parameter tuning on RF
+
+
+
+
+
         if self.regression:
-            # build NLP df with features
-
-
-
-            # take index from self.labels and build features based on prices
-
-
-
-            # merge the NLP features with the prices features
-
-
-
-            # Parameter tuning on RF
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -327,15 +269,15 @@ class Fab(object):
 
 
             models = [ModeClassifier(),
-                      randomForestClassifier(n_estimators=300)]
+                      RandomForestClassifier(n_estimators=300)]
 
 
         for ticker in tickers:
             if ticker in self.labels.keys():
-                print ticker
-                print "distribution of labels:"
+                print(ticker)
+                print("distribution of labels:")
                 for i, count in enumerate(np.bincount(self.labels[ticker]['binary'].values)):
-                    print "%d: %d" % (i, count)
+                    print("%d: %d" % (i, count))
                 if self.regression:
                     # min = self.labels[ticker]['pct-delta'].values.min()
                     # max = self.labels[ticker]['pct-delta'].values.max()
@@ -344,8 +286,3 @@ class Fab(object):
                 else:
                     self.compare_models(self.meeting_statements.loc[self.labels[ticker].index]['statements'].values, 
                                     self.labels[ticker]['binary'].values, models)
-
-
-if __name__ == '__main__':
-    fab = Fab()
-    fab.run()
